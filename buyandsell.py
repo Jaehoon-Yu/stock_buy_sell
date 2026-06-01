58e896969a941e12c48c21ba5522bb410f45ac56
Jaehoon-Yu <hh23420@bristol.ac.uk>
2026-06-01 12:35:14 +0900

    put orderbook

--- buyandsell.py
+++ buyandsell.py
@@ -1,5 +1,15 @@
-def __init__(self, price, order, ID, time_stamp):
-    self.price = price
-    self.order = order
-    self.ID = ID
-    
+# setting class
+class order:
+    def __init__(self, price, order, ID, time_stamp):
+        self.price = price
+        self.order = order
+        self.ID = ID
+        self.time_stamp = time_stamp
+
+class orderbook:
+    def __init__(self):
+        self.buy = []
+        self.sell = []
+        
+    self.buy.append({})
+        
